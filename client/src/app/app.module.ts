f8ac9f1ccc2b4c55fa22495ead6007535aa35bde
Mark Turner <nmdt3@roving-10-70-39-170.ncl.ac.uk>
2019-01-24 12:02:03 +0000

    Added map and timeline components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    TimelineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/translation/translation.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslationComponent } from './translation.component';
+
+@NgModule({
+  declarations: [TranslationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TranslationModule { }
