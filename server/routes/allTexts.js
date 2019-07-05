446af0dbcddf1e9ca94e5c1cc0e67d48468b39d5
KateCourt <nkc124@ncl.ac.uk>
2019-07-05 16:28:27 +0100

    env vars for google sheet

--- server/routes/allTexts.js
+++ server/routes/allTexts.js
@@ -8,15 +8,17 @@ const _ = require('lodash/core');
 //translations spreadsheet
 const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_NAME_TRANSLATIONS);
 
-//create JSON file for credentials
+//convert to JSON to remove extra \
 const tempCreds = JSON.stringify(creds, null, 2);
-const JSONcreds = tempCreds.replace(/\\\\n/g, "\\n")
+const JSONcreds = tempCreds.replace(/\\\\n/gm, "\\n");
+//convert back to js object
+const JScreds = JSON.parse(JSONcreds);
 
 /* GET all texts published in year provided e.g texts/1792 */
 router.get('/', function (req, res) {
     console.log("all texts route reached");
-    console.log(JSONcreds);
-    doc.useServiceAccountAuth(JSONcreds, function (err) {
+    console.log(JScreds);
+    doc.useServiceAccountAuth(JScreds, function (err) {
         if (err) {
             res.status(401);
             res.send('Service account access forbidden');
