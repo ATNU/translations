fe1714a60c6c82148ab0206e6f29918919f72329
KateCourt <nkc124@ncl.ac.uk>
2019-07-22 11:54:28 +0100

    update client_secret with env vars

--- server/routes/client_secret.js
+++ server/routes/client_secret.js
@@ -1,13 +1,13 @@
 module.exports =
 {
-  "type": "service_account",
-  "project_id": "translations-239508",
+  "type": process.env.PRIVATE_KEY_TYPE,
+  "project_id": process.env.PROJECT_ID,
   "private_key_id": process.env.PRIVATE_KEY_ID,
   "private_key": process.env.PRIVATE_KEY,
   "client_email": process.env.CLIENT_EMAIL,
   "client_id": process.env.CLIENT_ID,
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/translations-google-sheet%40translations-239508.iam.gserviceaccount.com"
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_CERT_URL
 }
